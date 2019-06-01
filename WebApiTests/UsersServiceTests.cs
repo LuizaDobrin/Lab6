4d3a7eab5c1e3620479a3f568f148b2fa39430a0
Luiza <luiza.dosa@gmail.com>
2019-06-01 10:08:36 +0300

    Tests added

--- WebApiTests/UsersServiceTests.cs
+++ WebApiTests/UsersServiceTests.cs
@@ -47,5 +47,76 @@ namespace Tests
                 Assert.AreEqual(added.Username, result.Username);
             }
         }
+
+        [Test]
+        public void GetAllShouldReturnAllRegisteredUsers() 
+        {
+            var options = new DbContextOptionsBuilder<TasksDbContext>()
+              .UseInMemoryDatabase(databaseName: nameof(GetAllShouldReturnAllRegisteredUsers))
+              .Options;
+
+            using (var context = new TasksDbContext(options))   
+            {
+                var usersService = new UsersService(context, config);
+                var added = new TaskAgendaProj.ViewModels.RegisterPostModel
+                {
+                    Email = "UserTest@test.com",
+                    FirstName = "FirstNameTest",
+                    LastName = "LastNameTest",
+                    Password = "1234567",
+                    Username = "test_username"
+                };
+                var newadded = new TaskAgendaProj.ViewModels.RegisterPostModel
+                {
+                    Email = "UserTest2@test.com",
+                    FirstName = "FirstNameTest2",
+                    LastName = "LastNameTest2",
+                    Password = "1234567890",
+                    Username = "test_username2"
+                };
+                usersService.Register(added);
+                usersService.Register(newadded);
+                int number = usersService.GetAll().Count();
+
+                Assert.IsNotNull(number);
+                Assert.AreEqual(2,number);
+            }
+        }
+
+        [Test]
+        public void AuthenticateShouldLogTheUser() 
+        {
+            var options = new DbContextOptionsBuilder<TasksDbContext>()
+              .UseInMemoryDatabase(databaseName: nameof(AuthenticateShouldLogTheUser))
+              .Options;
+
+            using (var context = new TasksDbContext(options))
+            {
+                var usersService = new UsersService(context, config);
+                var added = new TaskAgendaProj.ViewModels.RegisterPostModel
+                {
+                    Email = "UserTest3@test.com",
+                    FirstName = "FirstNameTest3",
+                    LastName = "LastNameTest3",
+                    Password = "12345678",
+                    Username = "test_username2"
+                };
+                var result = usersService.Register(added);
+
+                var authenticate=new TaskAgendaProj.ViewModels.LoginPostModel
+                {
+                    Username = "test_username2",
+                    Password = "12345678",
+                };
+
+                var authenticateresult = usersService.Authenticate(added.Username, added.Password);
+
+                Assert.IsNotNull(authenticateresult);
+                Assert.AreEqual(1, authenticateresult.Id);
+                Assert.AreEqual(authenticate.Username,authenticateresult.Username);
+            }
+        }
+
+
     }
 }
